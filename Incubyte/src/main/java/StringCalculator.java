33f14f585a96f11f8ca31ad9340f075002fd43a3
Gryphius2207 <hariprasadh2217@gmail.com>
2023-07-02 18:01:42 +0530

    Added method to handle an unknown amount of numbers

--- Incubyte/src/main/java/StringCalculator.java
+++ Incubyte/src/main/java/StringCalculator.java
@@ -16,6 +16,16 @@ public class StringCalculator {
         return total;
     }
 
+    public static int addNumbers(int... numbers) {
+        int total = 0;
+
+        for (int num : numbers) {
+            total += num;
+        }
+
+        return total;
+    }
+
     public static void main(String[] args) {
         // Test case: Empty string
         System.out.println(addNumbers(""));        // Output: 0
@@ -29,5 +39,10 @@ public class StringCalculator {
         System.out.println(addNumbers("1,2,3,4")); // Output: 10
         System.out.println(addNumbers("5,10,15")); // Output: 30
         System.out.println(addNumbers("100"));     // Output: 100
+
+        // Test case: Unknown amount of numbers
+        System.out.println(addNumbers(1, 2, 3, 4, 5));  // Output: 15
+        System.out.println(addNumbers(10, 20, 30));    // Output: 60
+        System.out.println(addNumbers(100, 200, 300)); // Output: 600
     }
 }
