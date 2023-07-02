5832214dfb7d2864828b2e9e28f434b9e96f9d02
Gryphius2207 <hariprasadh2217@gmail.com>
2023-07-02 18:05:35 +0530

    Added method to handle new lines between numbers (instead of commas).

--- Incubyte/src/main/java/StringCalculator.java
+++ Incubyte/src/main/java/StringCalculator.java
@@ -6,21 +6,11 @@ public class StringCalculator {
             return 0;
         }
 
-        String[] numArray = numbers.split(",");
+        String[] numArray = numbers.split("[\n,]");
         int total = 0;
 
         for (String num : numArray) {
             total += Integer.parseInt(num);
-        }
-
-        return total;
-    }
-
-    public static int addNumbers(int... numbers) {
-        int total = 0;
-
-        for (int num : numbers) {
-            total += num;
         }
 
         return total;
@@ -33,16 +23,11 @@ public class StringCalculator {
         // Test cases: One and two numbers
         System.out.println(addNumbers("1"));       // Output: 1
         System.out.println(addNumbers("2"));       // Output: 2
-        System.out.println(addNumbers("1,2"));     // Output: 3
+        System.out.println(addNumbers("1\n2"));    // Output: 3
 
         // Additional test cases
-        System.out.println(addNumbers("1,2,3,4")); // Output: 10
-        System.out.println(addNumbers("5,10,15")); // Output: 30
+        System.out.println(addNumbers("1\n2,3"));  // Output: 6
+        System.out.println(addNumbers("5\n10\n15"));// Output: 30
         System.out.println(addNumbers("100"));     // Output: 100
-
-        // Test case: Unknown amount of numbers
-        System.out.println(addNumbers(1, 2, 3, 4, 5));  // Output: 15
-        System.out.println(addNumbers(10, 20, 30));    // Output: 60
-        System.out.println(addNumbers(100, 200, 300)); // Output: 600
     }
 }
